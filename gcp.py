2e37f7c1d79b9000ba43639c985ae7d029edd501
Mustafa Alobaidi <a.todeto@gmail.com>
2021-12-15 18:33:29 -0500

    Add files via upload

---
+++ gcp.py
@@ -0,0 +1,18 @@
+from google.cloud import storage
+def hello_gcs(event, context):
+    file = event
+    bucket = file['bucket']
+    fileName = file['name']
+    try:
+         storage_client = storage.Client()
+         destination_bucket_name = 'cis4010-malobaidbackup'
+         src_bucket = storage_client.bucket(bucket)
+         destination_bucket = storage_client.bucket(destination_bucket_name)
+         src_object = src_bucket.blob(fileName)
+         destination_object_name = fileName
+         try:
+              copy_object = src_bucket.copy_blob(src_object,destination_bucket,destination_object_name)
+         except: 
+              print('failed to copy the file to the destination bucket: ', destination_bucket)
+    except:
+          print('failed to load the information from the bucket: ',bucket)
